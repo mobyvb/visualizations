dc0d847946b8d2cfafc5f41fd1c166b6e051c245
Moby <mobyvb@gmail.com>
2016-09-24 21:41:15 -0400

    add interactivity

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ var height = window.innerHeight - 50;
 canvas.width  = width;
 canvas.height = height;
 var ctx = canvas.getContext('2d');
+var radius1Control = document.getElementById('radius1');
+var radius2Control = document.getElementById('radius2');
+var offset1Control = document.getElementById('offset1');
+var offset2Control = document.getElementById('offset2');
+
 
 var circleResolution = 50;
 var innerCircle = {
@@ -24,7 +29,9 @@ var innerOffset = 0;
 var outerOffset = 0;
 var goingOut = true;
 var now = Date.now();
-window.requestAnimationFrame(frame);
+
+updateImage();
+// window.requestAnimationFrame(frame);
 function frame() {
   var then = now;
   now = Date.now();
@@ -61,10 +68,24 @@ function frame() {
     }
   }
   drawCircles(innerOffset, outerOffset);
-  window.requestAnimationFrame(frame);
+  // window.requestAnimationFrame(frame);
 }
 
+function updateImage() {
+  var innerOffset = 2*Math.PI * parseInt(offset1Control.value) / 100;
+  var outerOffset = 2*Math.PI * parseInt(offset2Control.value) / 100;
 
+  innerCircle.radius = parseInt(radius1Control.value);
+  outerCircle.radius = parseInt(radius2Control.value);
+  console.log(innerOffset);
+  console.log(outerOffset);
+  console.log(innerCircle.radius);
+  console.log(outerCircle.radius);
+  drawCircles(innerOffset, outerOffset);
+  window.requestAnimationFrame(function() {
+    drawCircles(innerOffset, outerOffset);
+  });
+}
 
 function drawCircles(offsetInner, offsetOuter) {
   ctx.clearRect(0, 0, width, height);
@@ -74,6 +95,7 @@ function drawCircles(offsetInner, offsetOuter) {
       y: height / 2
     };
     var currPercentage = i / circleResolution;
+    var currColor = colorWheel(255 * currPercentage);
     var currAngleInner = 2 * Math.PI * currPercentage + offsetInner;
     var currAngleOuter = 2 * Math.PI * currPercentage + offsetOuter;
     var nextInnerPoint = {
@@ -84,9 +106,24 @@ function drawCircles(offsetInner, offsetOuter) {
       x: center.x + Math.cos(currAngleOuter) * outerCircle.radius,
       y: center.y + Math.sin(currAngleOuter) * outerCircle.radius
     }
+
+    ctx.strokeStyle = 'rgb(' + currColor[0] + ',' + currColor[1] + ',' + currColor[2] + ')';
     ctx.beginPath();
     ctx.moveTo(nextInnerPoint.x, nextInnerPoint.y);
     ctx.lineTo(nextOuterPoint.x, nextOuterPoint.y);
     ctx.stroke();
   }
+}
+
+// position must be between 0 and 255
+function colorWheel(position) {
+  var wheelPos = 255 - position;
+  if (wheelPos < 85) {
+    return [255 - wheelPos * 3, 0, wheelPos * 3];
+  }
+  if (wheelPos < 170) {
+    return [0, wheelPos * 3, 255 - wheelPos * 3];
+  }
+  wheelPos -= 170;
+  return [wheelPos * 3, 255 - wheelPos * 3, 0];
 }
